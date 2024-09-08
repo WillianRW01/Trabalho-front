2e8a4c55c8e7fe1d72a2b5aeef1c29e42d7edb4a
Willian <willianramoswilgen@gmai.com>
2024-09-07 23:44:14 -0300

    Organização-do-codigo

--- src/pages/Pokedex/index.jsx
+++ src/pages/Pokedex/index.jsx
@@ -1,37 +1,19 @@
 import React, { useEffect, useState } from 'react';
-
-// Mapeamento de tipos em inglês para português
-const typeTranslations = {
-  normal: "Normal",
-  fire: "Fogo",
-  water: "Água",
-  grass: "Grama",
-  electric: "Elétrico",
-  ice: "Gelo",
-  fighting: "Lutador",
-  poison: "Venenoso",
-  ground: "Terra",
-  flying: "Voador",
-  psychic: "Psíquico",
-  bug: "Inseto",
-  rock: "Pedra",
-  ghost: "Fantasma",
-  dragon: "Dragão",
-  dark: "Sombrio",
-  steel: "Metálico",
-  fairy: "Fada",
-};
+import './styles.css'; 
+import { typeTranslations, typeColors } from './typeMappings'; 
 
 const PokemonList = () => {
   const [pokemons, setPokemons] = useState([]);
   const [filteredPokemons, setFilteredPokemons] = useState([]);
   const [search, setSearch] = useState('');
+  const [favorites, setFavorites] = useState([]); 
+  const [selectedPokemon, setSelectedPokemon] = useState(null); 
 
   // Função para buscar detalhes de cada Pokémon
   const fetchPokemonDetails = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
-    
+
     return {
       name: data.name,
       image: data.sprites.front_default, // Imagem frontal do Pokémon
@@ -41,11 +23,14 @@ const PokemonList = () => {
       abilities: data.abilities
         .map((abilityInfo) => abilityInfo.ability.name.replace('-', ' ')) // Remover hífens nas habilidades
         .join(', '), // Junta as habilidades em uma string
+      height: data.height, // Altura do Pokémon
+      weight: data.weight, // Peso do Pokémon
+      typeArray: data.types.map((typeInfo) => typeTranslations[typeInfo.type.name] || typeInfo.type.name) // Array de tipos
     };
   };
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=500')
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=999')
       .then((response) => response.json())
       .then(async (data) => {
         const detailedPokemons = await Promise.all(
@@ -65,6 +50,31 @@ const PokemonList = () => {
     setFilteredPokemons(filtered);
   };
 
+  // Função para alternar entre favorito e não favorito
+  const toggleFavorite = (pokemon) => {
+    setFavorites((prevFavorites) =>
+      prevFavorites.includes(pokemon)
+        ? prevFavorites.filter((fav) => fav.name !== pokemon.name)
+        : [...prevFavorites, pokemon]
+    );
+  };
+
+  // Função para determinar a cor de fundo com base no primeiro tipo do Pokémon
+  const getBackgroundColor = (types) => {
+    const mainType = types[0]; // Usando o primeiro tipo como principal
+    return typeColors[mainType] || '#FFF'; // Cor padrão se não houver cor associada
+  };
+
+  // Função para abrir o modal com detalhes do Pokémon
+  const openModal = (pokemon) => {
+    setSelectedPokemon(pokemon);
+  };
+
+  // Função para fechar o modal
+  const closeModal = () => {
+    setSelectedPokemon(null);
+  };
+
   return (
     <div>
       <h1>Pokédex</h1>
@@ -76,45 +86,88 @@ const PokemonList = () => {
         onChange={handleSearch}
       />
 
-      <div className="pokemon-grid">
-        {filteredPokemons.map((pokemon, index) => (
-          <div key={index} className="pokemon-card">
-            <img src={pokemon.image} alt={pokemon.name} />
-            <h3>{pokemon.name}</h3>
-            <p>Tipos: {pokemon.types}</p>
-            <p>Habilidades: {pokemon.abilities}</p> {/* Exibe as habilidades */}
+      {favorites.length > 0 && (
+        <div>
+          <h2>Pokémon Favoritos</h2>
+          <div className="pokemon-grid">
+            {favorites.map((pokemon, index) => (
+              <div 
+                key={index} 
+                className="pokemon-card favorite"
+                style={{ backgroundColor: getBackgroundColor(pokemon.typeArray) }} // Cor de fundo dinâmica
+                onClick={() => openModal(pokemon)}
+              >
+                <img src={pokemon.image} alt={pokemon.name} />
+                <h3>{pokemon.name}</h3>
+                <p>Tipos: {pokemon.types}</p>
+                <p>Habilidades: {pokemon.abilities}</p>
+                <button
+                  className="favorite-button"
+                  onClick={(e) => {
+                    e.stopPropagation(); 
+                    toggleFavorite(pokemon);
+                  }}
+                  style={{ backgroundColor: 'red' }}
+                >
+                  Desfavoritar
+                </button>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
+      )}
+
+     
+      <div className="pokemon-grid">
+        {filteredPokemons
+          .filter((pokemon) => !favorites.includes(pokemon)) 
+          .map((pokemon, index) => (
+            <div 
+              key={index} 
+              className="pokemon-card"
+              style={{ backgroundColor: getBackgroundColor(pokemon.typeArray) }} 
+              onClick={() => openModal(pokemon)}
+            >
+              <img src={pokemon.image} alt={pokemon.name} />
+              <h3>{pokemon.name}</h3>
+              <p>Tipos: {pokemon.types}</p>
+              <p>Habilidades: {pokemon.abilities}</p>
+              <button
+                className="favorite-button"
+                onClick={(e) => {
+                  e.stopPropagation(); 
+                  toggleFavorite(pokemon);
+                }}
+              >
+                Favoritar
+              </button>
+            </div>
+          ))}
       </div>
 
-      <style jsx>{`
-        .pokemon-grid {
-          display: grid;
-          grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-          gap: 20px;
-          margin-top: 20px;
-        }
-
-        .pokemon-card {
-          border: 1px solid #ccc;
-          padding: 10px;
-          border-radius: 8px;
-          text-align: center;
-        }
-
-        img {
-          width: 100px;
-          height: 100px;
-        }
-
-        input {
-          width: 100%;
-          padding: 10px;
-          margin-top: 10px;
-          border-radius: 8px;
-          border: 1px solid #ccc;
-        }
-      `}</style>
+    
+      {selectedPokemon && (
+        <div className="modal">
+          <div className="modal-background" onClick={closeModal}></div>
+          <div className="modal-content">
+            <h2>{selectedPokemon.name}</h2>
+            <img src={selectedPokemon.image} alt={selectedPokemon.name} />
+            <p>Tipos: {selectedPokemon.types}</p>
+            <p>Habilidades: {selectedPokemon.abilities}</p>
+            <p>Altura: {selectedPokemon.height}</p>
+            <p>Peso: {selectedPokemon.weight}</p>
+            <p>Regiões: {selectedPokemon.locations}</p> 
+            <button
+              className="favorite-button"
+              onClick={() => toggleFavorite(selectedPokemon)}
+              style={{ backgroundColor: favorites.includes(selectedPokemon) ? 'red' : '#ffcc00' }}
+            >
+              {favorites.includes(selectedPokemon) ? 'Desfavoritar' : 'Favoritar'}
+            </button>
+            <button className="close-button" onClick={closeModal}>Fechar</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/pages/Pokedex/typeMappings.js
@@ -0,0 +1,46 @@
+// typeMappings.js
+
+// Mapeamento de tipos em inglês para português
+export const typeTranslations = {
+    normal: "Normal",
+    fire: "Fogo",
+    water: "Água",
+    grass: "Grama",
+    electric: "Elétrico",
+    ice: "Gelo",
+    fighting: "Lutador",
+    poison: "Venenoso",
+    ground: "Terra",
+    flying: "Voador",
+    psychic: "Psíquico",
+    bug: "Inseto",
+    rock: "Pedra",
+    ghost: "Fantasma",
+    dragon: "Dragão",
+    dark: "Sombrio",
+    steel: "Metálico",
+    fairy: "Fada",
+  };
+  
+  // Mapeamento de cores para cada tipo de Pokémon
+  export const typeColors = {
+    Normal: "#A8A77A",
+    Fogo: "#EE8130",
+    Água: "#6390F0",
+    Grama: "#7AC74C",
+    Elétrico: "#F7D02C",
+    Gelo: "#96D9D6",
+    Lutador: "#C22E28",
+    Venenoso: "#A33EA1",
+    Terra: "#E2BF65",
+    Voador: "#A98FF3",
+    Psíquico: "#F95587",
+    Inseto: "#A6B91A",
+    Pedra: "#B6A136",
+    Fantasma: "#735797",
+    Dragão: "#6F35FC",
+    Sombrio: "#705746",
+    Metálico: "#B7B7CE",
+    Fada: "#D685AD",
+  };
+  
