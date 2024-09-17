d5bde881c60d00768ab7bb36913b82adb8278472
João Vitor Philippe <joaophilippe50@gmail.com>
2024-09-16 21:52:28 -0300

    feat: criação backend

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const userRouter = require('./src/routes/user');
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/v1/user", userRouter)
+
+app.listen(3000, () => {
+    console.log('servidor rodando na porta 3000')
+})
