eab860f41b73b81f577cec73b7240c9a67aace2c
Willian <willianramoswilgen@gmai.com>
2024-11-09 01:04:28 -0300

    mais corrções

--- front-end/src/pages/Pokelist/index.jsx
+++ front-end/src/pages/Pokelist/index.jsx
@@ -1,18 +1,17 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './styles.css';
 import PokemonCard from '../../components/PokemonCard/PokemonCard.jsx';
 import SearchBar from '../../components/SearchBar/SearchBar.jsx';
 import Pagination from '../../components/Pagination/Pagination.jsx';
-import { listarPokemons } from '../../api/pokemon.jsx';
-import { AuthContext } from '../../auth/Context';
+import { listarPokemons, deletarPokemon } from '../../api/pokemon.jsx';
 
 const PokemonList = () => {
-  const { role } = useContext(AuthContext); 
   const [pokemons, setPokemons] = useState([]);
   const [search, setSearch] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 25;
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchPokemons = async () => {
@@ -22,9 +21,24 @@ const PokemonList = () => {
     fetchPokemons();
   }, []);
 
+  const handleDeletePokemon = async (id) => {
+    try {
+      await deletarPokemon(id);
+      setPokemons((prevPokemons) => prevPokemons.filter((p) => p.id !== id));
+    } catch (error) {
+      console.error('Erro ao excluir Pokémon:', error);
+    }
+  };
+
+  const handleEditPokemon = (id) => {
+    navigate(`/pokemon/edit/${id}`);
+  };
+
+
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
   };
+
 
   const filteredPokemons = pokemons.filter((pokemon) =>
     pokemon.nome.toLowerCase().includes(search.toLowerCase())
@@ -37,11 +51,9 @@ const PokemonList = () => {
     <div>
       <center>
         <h1>Pokédex</h1>
-        {role === 'admin' && ( // Exibe o botão "Criar Pokémon" apenas se o role for "admin"
-          <Link to="/pokemon/new">
-            <button>Criar Pokémon</button>
-          </Link>
-        )}
+        <Link to="/pokemon/new">
+          <button>Criar Pokémon</button>
+        </Link>
       </center>
       <SearchBar search={search} handleSearch={(e) => setSearch(e.target.value)} />
       <div className="pokemon-grid">
@@ -49,6 +61,8 @@ const PokemonList = () => {
           <PokemonCard
             key={pokemon.id}
             pokemon={pokemon}
+            onEdit={() => handleEditPokemon(pokemon.id)}
+            onDelete={handleDeletePokemon}
           />
         ))}
       </div>
