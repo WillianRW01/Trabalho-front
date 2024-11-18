c1e73172f69981206afd0f2eee22aa20897e5563
João Vitor Philippe <joaophilippe50@gmail.com>
2024-11-18 19:37:23 -0300

    feat: finalização botão de logout

--- front-end/src/components/Logout/index.jsx
+++ front-end/src/components/Logout/index.jsx
@@ -1,9 +1,9 @@
 import { useContext } from 'react';
-import './styles.css'
+import './styles.css';
 import { AuthContext } from '../../auth/Context';
 
 export default function LogoutButton() {
-const { logout } = useContext(AuthContext)
+  const { logout } = useContext(AuthContext);
 
-  return <button onClick={logout}>Logout</button>;
+  return <button className="logout-button" onClick={logout}>Logout</button>;
 }
